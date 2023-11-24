f2abe762a6936d8d320148004a47692a92646adc
hartmann <hartmann@appc66.psychologie.privat>
2023-11-24 14:09:06 +0100

    added basic files

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    bashInteractive
+    rEnv
+    rstudioEnv
+    gsl
+    git
+    # ... any other dependencies
+  ];
+
+  # You can set environment variables as needed
+  # environment variables that should be set when the shell is entered
+  shellHook = ''
+    echo "entering rEnv rstudioEnv and gsl evnironment"
+    mkdir -p "$(pwd)/_libs"
+    export R_LIBS_USER="$(pwd)/_libs"
+    #  export SOME_VAR=some_value
+  '';
+}
